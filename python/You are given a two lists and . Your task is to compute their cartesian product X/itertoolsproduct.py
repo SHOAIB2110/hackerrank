b12fa346e5fce439b0704629c5cf6df9894ad083
SHOAIB2110 <56501162+SHOAIB2110@users.noreply.github.com>
2020-04-23 23:50:09 +0530

    itertools.product()

---
+++ python/You are given a two lists and . Your task is to compute their cartesian product X/itertoolsproduct.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+print(*product(a,b))
