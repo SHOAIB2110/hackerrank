0b8098bfcef43061288ad9cf8e041f9acfcea504
SHOAIB2110 <56501162+SHOAIB2110@users.noreply.github.com>
2021-04-29 00:41:51 +0530

    Create Weighted Mean.py

---
+++ 10 Days of Statistics/Day0/Weighted Mean.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    # Write your code here
+    prod=[X[i]*W[i] for i in range(len(X))]
+    sProd=sum(prod)
+    wSum=sum(W)
+    weightedMean=sProd/wSum
+    print(round(weightedMean,1))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
